aacf601c818bd05dc016253ac3cc00e079d4c966
Hagrass <bodyhagrass@gmail.com>
2022-03-15 02:38:37 +0200

    Tweets extracted done (contentScript in build )

--- build/contentScript.js
+++ build/contentScript.js
@@ -10,10 +10,25 @@ window.addEventListener('load', function () {
     for (var el of elements)
     {
         var spans = el.getElementsByTagName('span');
-        for (var text of spans)
-        {
-            console.log(text.innerText)
-        }
+
+        let  total_text="";
+                
+       // To avoid taking user name from spans
+       let userInfo=0;
+       for (var text of spans)
+       {
+           if(userInfo<=3)
+           {
+               userInfo++;
+               continue;
+           }
+           
+          
+           total_text = total_text.concat(text.innerText);
+       }
+       console.log("Tweet: ",total_text)
+      
+
     }
 
     /* MutationObserver callback to add spans when the body changes */
@@ -21,14 +36,27 @@ window.addEventListener('load', function () {
        
         for( var mutation of mutationsList ) {
            
-            if(mutation.target.tagName==='ARTICLE')
+            if(mutation.target.tagName==='ARTICLE'&&mutation.attributeName==='role')
             {
-        
+                
                 var spans = mutation.target.getElementsByTagName('span');
+                let  total_text="";
+                
+                // To avoid taking user name from spans
+                let userInfo=0;
                 for (var text of spans)
                 {
-                    console.log(text.innerText)
+                    if(userInfo<=3)
+                    {
+                        userInfo++;
+                        continue;
+                    }
+                    
+                   
+                    total_text = total_text.concat(text.innerText);
                 }
+                console.log("Tweet: ",total_text)
+                console.log("Type :",mutation)
 
                 
             }
