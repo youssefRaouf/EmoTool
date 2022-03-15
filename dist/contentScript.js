a58f7d9ea825a65946d1ed1353102f759bcda57d
Hagrass <bodyhagrass@gmail.com>
2022-03-15 02:02:38 +0200

    Extracted Text of Tweets in spans

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -1,38 +1,44 @@
-var spans = []
 
-console.log("In AA contentScript")
+console.log("In  contentScript")
 window.addEventListener('load', function () {
     console.log("Loaded ")
-    var body = document.body;
+    var body = document.body;    
+    
+    
     let elements = document.getElementsByTagName('article')
 
-    console.log(elements.length)
-    Array.from(elements).forEach(function (element) {
-        console.log(element)
-      });
-    
-    
+    for (var el of elements)
+    {
+        var spans = el.getElementsByTagName('span');
+        for (var text of spans)
+        {
+            console.log(text.innerText)
+        }
+    }
 
     /* MutationObserver callback to add spans when the body changes */
     var callback = function( mutationsList, observer ){
+       
         for( var mutation of mutationsList ) {
-            if ( mutation.type === 'childList' ) {
-               
-                Array.prototype.forEach.call( mutation.target.children, function ( child ) {
-                   
-                    if(child.tagName ==='ARTICLE')  {
-                       
-                        spans.push(child);
-                        console.log(child);
-                        console.log("Action happened",spans.length);
-                    } 
-                } );
+           
+            if(mutation.target.tagName==='ARTICLE')
+            {
+        
+                var spans = mutation.target.getElementsByTagName('span');
+                for (var text of spans)
+                {
+                    console.log(text.innerText)
+                }
+
+                
             }
+            
+           
         }
     }
     var observer = new MutationObserver( callback );
     var config = { characterData: true,
-                attributes: false,
+                attributes: true,
                 childList: true,
                 subtree: true };
 
