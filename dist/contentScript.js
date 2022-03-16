78ffea646e6fb139b234275ace143b1e511c6d9d
youssef raouf <youssefraouf32@gmail.com>
2022-03-16 19:55:33 +0200

    tweets extraction final

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -1,74 +1,55 @@
-
-console.log("In  contentScript")
 window.addEventListener('load', function () {
     console.log("Loaded ")
-    var body = document.body;    
-    
-    
-    let elements = document.getElementsByTagName('article')
-
-    for (var el of elements)
-    {
-        var spans = el.getElementsByTagName('span');
-
-        let  total_text="";
-                
-       // To avoid taking user name from spans
-       let userInfo=0;
-       for (var text of spans)
-       {
-           if(userInfo<=3)
-           {
-               userInfo++;
-               continue;
-           }
-           
-          
-           total_text = total_text.concat(text.innerText);
-       }
-       console.log("Tweet: ",total_text)
-      
-
-    }
-
-    /* MutationObserver callback to add spans when the body changes */
-    var callback = function( mutationsList, observer ){
-       
-        for( var mutation of mutationsList ) {
-           
-            if(mutation.target.tagName==='ARTICLE'&&mutation.attributeName==='role')
-            {
-                
-                var spans = mutation.target.getElementsByTagName('span');
-                let  total_text="";
-                
-                // To avoid taking user name from spans
-                let userInfo=0;
-                for (var text of spans)
-                {
-                    if(userInfo<=3)
-                    {
-                        userInfo++;
-                        continue;
-                    }
-                    
-                   
-                    total_text = total_text.concat(text.innerText);
+    let tweetsSection = document.getElementsByTagName('section')[0]
+    setTimeout(() => {
+        tweetsSection = document.getElementsByTagName('section')[0]
+        const elements = document.getElementsByTagName('article')
+        const tweets = []
+        for (const el of elements) {
+            const spans = el.getElementsByTagName('span');
+            let total_text = "";
+            // To avoid taking user name from spans
+            let userInfo = 0;
+            for (const text of spans) {
+                if (userInfo <= 3) {
+                    userInfo++;
+                    continue;
                 }
-                console.log("Tweet: ",total_text)
-                console.log("Type :",mutation)
-
-                
+                total_text = total_text.concat(text.innerText);
             }
-            
-           
+            tweets.push({ text: total_text, label: null })
         }
-    }
-    var observer = new MutationObserver( callback );
-    var config = { characterData: true,
-                attributes: true,
-                childList: true,
-                subtree: true };
 
-    observer.observe( body, config );
+        /* MutationObserver callback to add spans when the body changes */
+        const callback = function (mutationsList, observer) {
+            for (const mutation of mutationsList) {
+                if (mutation.target.tagName === 'ARTICLE' && mutation.attributeName === 'role') {
+                    const spans = mutation.target.getElementsByTagName('span');
+                    let total_text = "";
+                    // To avoid taking user name from spans
+                    let userInfo = 0;
+                    for (const text of spans) {
+                        if (userInfo <= 3) {
+                            userInfo++;
+                            continue;
+                        }
+                        total_text = total_text.concat(text.innerText);
+                    }
+                    if (!tweets.find((tweet) => tweet.text === total_text)) {
+                        tweets.push({ text: total_text, label: null })
+                        console.log(tweets)
+                    }
+                }
+            }
+        }
+        const observer = new MutationObserver(callback);
+        const config = {
+            attributes: true,
+            subtree: true,
+            attributeOldValue: true,
+            attributeFilter: ['role']
+        };
+        observer.observe(tweetsSection, config);
+    }, 3000)
+
 });
